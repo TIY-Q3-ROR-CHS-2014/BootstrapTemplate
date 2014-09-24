4c0de5dbdd57e5ae6b181cc72a1f8cf4ac1d8461
Nick Bucciarelli <nick.bucciarelli@gmail.com>
2014-09-24 11:31:55 -0400

    First commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+  # To use a different layout for this controller
+  # create a views/layoutes/dudeman.html.erb
+  # layout :dudeman
+
+  # Alternate way:
+  # welcome.html.erb
+  def index
+  end
+end
